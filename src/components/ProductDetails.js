8f96cabf9600f3f3782dc10adafdc3bcfcba4c17
A1cy <a1hvdy@gmail.com>
2023-09-25 11:46:20 +0300

    a

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import '@splidejs/splide/dist/css/splide.min.css';
+import Splide from '@splidejs/splide';
 
-// Product Details Component
 const ProductDetails = () => {
-   return (
-      <div className="content-container">
-        <div className="images-container">
-          <section id="image-carousel" className="splide">
-            <div className="splide__track">
-              <ul className="splide__list">
-                <li className="splide__slide"><img src="/pics/product1.png" alt="product pic" /></li>
-                {/* Add more <li> here for more images */}
-              </ul>
-            </div>
-          </section>
-          <section id="thumbnail-carousel" className="splide">
-            <div className="splide__track">
-              <ul className="splide__list">
-                <li className="splide__slide"><img src="/pics/product1.png" alt="product pic" /></li>
-                {/* Add more <li> here for more images */}
-              </ul>
-            </div>
-          </section>
-        </div>
-        <div className="product-content">
+
+  React.useEffect(() => {
+    new Splide('#image-carousel', {
+      type  : 'fade',
+      arrows: true,
+    }).mount();
+    
+    new Splide('#thumbnail-carousel').mount();
+  }, []);
+
+  return (
+    <div className="content-container">
+      <div className="images-container">
+        <section id="image-carousel" className="splide splide--fade splide--rtl splide--draggable is-active is-initialized">
+          <div className="splide__track">
+            <ul className="splide__list">
+              {/* Add your images here */}
+              <li className="splide__slide"><img src="/pics/product1.png" alt="product pic"/></li>
+              {/* ... */}
+            </ul>
+          </div>
+        </section>
+        <section id="thumbnail-carousel" className="splide splide--slide splide--rtl splide--draggable splide--nav is-active is-initialized">
+          <div className="splide__track">
+            <ul className="splide__list">
+              {/* Add your thumbnails here */}
+              <li className="splide__slide"><img src="/pics/product1.png" alt="product pic"/></li>
+              {/* ... */}
+            </ul>
+          </div>
+        </section>
+      </div>
+      <div className="product-content">
           <h1 id="product-name">Angle valve - AVE002</h1>
           <div className="inline">
             <img className="tiny-logo" src="/pics/Hatch-logo.png" alt="brand logo" />
@@ -38,8 +51,7 @@ const ProductDetails = () => {
             <button className="Lost-a-Sale" id="Lost-a-Sale">Lost a Sale</button>
           </div>
         </div>
-      </div>
-    );
-  };
-
+    </div>
+  );
+};
 export default ProductDetails;
