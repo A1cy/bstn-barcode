9f576070ce20a409bf6e241d0f1fdc8db4b1ab43
A1cy <a1hvdy@gmail.com>
2023-10-08 13:52:10 +0300

    z

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import QrReader from 'react-qr-reader';
+// import QrReader from 'react-qr-reader';
 
 function Footer() {
   const [showScanner, setShowScanner] = useState(false);
