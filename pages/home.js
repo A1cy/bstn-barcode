e0d7b781bd9676bbf42e34403519177e67e2e435
A1cy <a1hvdy@gmail.com>
2023-10-09 10:07:53 +0300

    tt

--- pages/home.js
+++ pages/home.js
@@ -1,15 +1,13 @@
-import React, { useState, useMemo } from "react";
+
+import React, { useState } from "react";
 import axios from "axios";
+import { useZxing } from 'react-zxing';
 
 export default function Home() {
   const [sku, setSku] = useState("");
   const [error, setError] = useState(null);
   const [productDetail, setProductDetail] = useState(null);
   const [showScanner, setShowScanner] = useState(false);
-
-  const BarcodeScanner = useMemo(() => {
-    return import("react-qr-barcode-scanner");
-  }, []);
 
   const handleSkuChange = (e) => {
     setSku(e.target.value); 
@@ -18,40 +16,33 @@ export default function Home() {
   const handleSearch = (e) => {
     e.preventDefault(); // Prevent default form submission
     axios.post('/api/get-product', { sku })
+        // ... (rest of your axios calls)
         .then(response => {
-            if (response.data && response.data.slug && response.data.category) {
-                return axios.get(`/api/get-product-detail?slug=${response.data.slug}&category=${response.data.category}`);
-            } else {
-                throw new Error('Product not found');
-            }
-        })
-        .then(detailResponse => {
-            setProductDetail(detailResponse.data);
-        })
-        .catch(err => {
-            console.error(err);
-            if (err.message === 'Product not found' || (err.response && err.response.status === 404)) {
-                setError('Product not found');
-            } else {
-                setError('Failed to fetch product');
-            }
-        });
+          if (response.data && response.data.slug && response.data.category) {
+              return axios.get(`/api/get-product-detail?slug=${response.data.slug}&category=${response.data.category}`);
+          } else {
+              throw new Error('Product not found');
+          }
+      })
+      .then(detailResponse => {
+          setProductDetail(detailResponse.data);
+      })
+      .catch(err => {
+          console.error(err);
+          if (err.message === 'Product not found' || (err.response && err.response.status === 404)) {
+              setError('Product not found');
+          } else {
+              setError('Failed to fetch product');
+          }
+      });
   };
 
-  const handleError = (err) => {
-    console.error(err);
-    setError("Error during scanning");
-  };
-
-  const handleScan = (result) => {
-    setSku(result);
+  const onScan = code => {
+    setSku(code.data);
     handleSearch();
   };
 
-  // Define handleUpdate function or remove onUpdate={handleUpdate}
-  const handleUpdate = (update) => {
-    // handle updates if necessary
-  };
+  const { loading, error: scannerError, scan } = useZxing({ onScan });
 
   return (
     <div className="container">
@@ -61,7 +52,7 @@ export default function Home() {
       <main className="home-main">
         <form onSubmit={handleSearch} className="sku-form">
           <input type="text" name="sku" value={sku} onChange={handleSkuChange} />
-          <button type="button" className="scan-button" onClick={e => {e.preventDefault(); setShowScanner(true)}}>Scan Barcode</button>
+          <button type="button" className="scan-button" onClick={() => setShowScanner(true)}>Scan Barcode</button>
         </form>
         {error && <p>{error}</p>}
         {productDetail && productDetail.item && (
@@ -75,15 +66,8 @@ export default function Home() {
       {showScanner && (
         <div className="scanner-modal">
           <div className="scanner-content">
-            {typeof window !== 'undefined' && (
-              <BarcodeScanner.Scanner
-                delay={300}
-                onError={handleError}
-                onScan={handleScan}
-                onUpdate={handleUpdate} // handleUpdate is now defined
-                style={{ width: '100%' }}
-              />
-            )}
+            {loading ? <p>Loading...</p> : <video ref={scan} />}
+            {scannerError && <p>Error: {scannerError}</p>}
             <button className="close-button" onClick={() => setShowScanner(false)}>Close</button>
           </div>
         </div>
