67b5ccdcdc73ba8303d97ed0b10f01f3f815151a
A1cy <a1hvdy@gmail.com>
2023-10-10 11:44:51 +0300

    tbwee

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,6 @@
-
-
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { Html5Qrcode } from 'html5-qrcode';
-import { Html5QrcodeSupportedFormats } from "html5-qrcode";
+import jsQR from "jsqr";
 import Image from 'next/image';
 
 export default function Home() {
@@ -11,17 +8,17 @@ export default function Home() {
   const [error, setError] = useState(null);
   const [productDetail, setProductDetail] = useState(null);
   const [showScanner, setShowScanner] = useState(false);
-  const scannerRef = useRef(null);
-
-  const [scannerActive, setScannerActive] = useState(false); // New state variable
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
   const handleSkuChange = (e) => {
     setSku(e.target.value);
   };
 
-  const handleSearch = (e) => {
+  const handleSearch = (e, scannedSku) => {
+    const targetSku = scannedSku || sku;
     if (e) e.preventDefault();
-    axios.post('/api/get-product', { sku })
+    axios.post('/api/get-product', { sku: targetSku })
       .then(response => {
         if (response.data && response.data.slug && response.data.category) {
           return axios.get(`/api/get-product-detail?slug=${response.data.slug}&category=${response.data.category}`);
@@ -43,112 +40,89 @@ export default function Home() {
       });
   };
 
-  useEffect(() => {
-    const scanner = scannerRef.current;
-    console.log("Scanner element:", scanner); 
-    let html5QrCode;
-    if (showScanner && scanner) {
-      console.log("Scanner dimensions:", scanner.offsetWidth, scanner.offsetHeight);  // Log dimensions
-      Html5Qrcode.getCameras().then(devices => {
-        console.log("Devices found:", devices);  // Debugging line
-        if (devices && devices.length) {
-          // ... (camera selection logic)
-          const backCamera = devices.find(device => device.label.toLowerCase().includes('back'));
-          const cameraId = backCamera ? backCamera.id : devices[0].id;
-          console.log("Using camera:", cameraId);  // Debugging line
-          html5QrCode = new Html5Qrcode(scanner.id);
-          html5QrCode.start(
-            cameraId,
-            {
-              // ... (scanner configuration)
-              fps: 15,
-              qrbox: 300,
-              aspectRatio: 1.0, // Set aspect ratio to be square
-              formatsToSupport: [ 
-                Html5QrcodeSupportedFormats.QR_CODE,
-                Html5QrcodeSupportedFormats.CODE128,
-                Html5QrcodeSupportedFormats.CODE39,
-                Html5QrcodeSupportedFormats.CODE93,
-                Html5QrcodeSupportedFormats.CODABAR,
-                Html5QrcodeSupportedFormats.DATA_MATRIX,
-                Html5QrcodeSupportedFormats.EAN13,
-                Html5QrcodeSupportedFormats.EAN8,
-                Html5QrcodeSupportedFormats.ITF,
-                Html5QrcodeSupportedFormats.MAXICODE,
-                Html5QrcodeSupportedFormats.PDF417,
-                Html5QrcodeSupportedFormats.RSS14,
-                Html5QrcodeSupportedFormats.RSSEXPANDED,
-                Html5QrcodeSupportedFormats.UPCA,
-                Html5QrcodeSupportedFormats.UPCE,
-                Html5QrcodeSupportedFormats.UPCEAN_EXTENSION,
-              ]
-            },
-            qrCodeMessage => {
-              console.log("QR Code scanned:", qrCodeMessage);  // Debugging line
-              setSku(qrCodeMessage);
-              handleSearch();
-              if (html5QrCode && scannerActive) { // Conditional stop
-                html5QrCode.stop().then(() => {
-                  setScannerActive(false); // Set scanner inactive upon successful stop
-                }).catch(err => {
-                  console.error("Error stopping the scanner", err);
-                });
-              }
-              setShowScanner(false);
-            },
-            errorMessage => {
-              console.error("Scanner error:", errorMessage);
-              setError(errorMessage);
-            }
-          ).then(() => {
-            setScannerActive(true); // Set scanner active upon successful start
-          }).catch(err => {
-            setError(err);
-          });
-        }
-      }).catch(err => {
-        setError("Error initializing scanner");
+  const startScanner = () => {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+        requestAnimationFrame(tick);
+      })
+      .catch(err => {
+        setError("Failed to access camera");
       });
-    }
-    return () => {
-      if (html5QrCode && scannerActive) { // Conditional stop
-        html5QrCode.stop().then(() => {
-          setScannerActive(false); // Set scanner inactive upon successful stop
-        }).catch(err => {
-          console.error("Error stopping the scanner", err);
-        });
+  };
+
+  const tick = () => {
+    if (videoRef.current && videoRef.current.readyState === videoRef.current.HAVE_ENOUGH_DATA) {
+      const canvas = canvasRef.current;
+      const video = videoRef.current;
+      const context = canvas.getContext("2d");
+      canvas.height = video.videoHeight;
+      canvas.width = video.videoWidth;
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
+      if (code) {
+        setSku(code.data);
+        handleSearch();
+        setShowScanner(false);
+        videoRef.current.pause();
+        if (videoRef.current && videoRef.current.srcObject) {
+          videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+        }
+      } else {
+        requestAnimationFrame(tick);
       }
-    };
-  }, [showScanner, handleSearch]);
+    } else {
+      if(videoRef.current) {
+        requestAnimationFrame(tick);
+      }
+    }
+  };
+  
+  useEffect(() => {
+    if (showScanner) {
+      startScanner();
+    }
+  }, [showScanner]);
+  
 
   // ... (render logic)
   return (
     <div className="container">
-      <header className="home-header">
-        <Image src="/pics/BuildStation-logo.png" alt="Logo" width={150} height={150} className="logo"/> {/* Updated */}
-      </header>
-      <main className="home-main">
-        <form onSubmit={handleSearch} className="sku-form">
-          <input type="text" name="sku" value={sku} onChange={handleSkuChange} />
-          <button type="button" className="scan-button" onClick={() => setShowScanner(true)}>Scan Barcode</button>
-        </form>
-        {error && <p>{error}</p>}
-        {productDetail && productDetail.item && (
-          <div>
-            <h2>{productDetail.item.title}</h2>
-            <Image src={productDetail.item.image} alt={productDetail.item.title} width={150} height={150}/> {/* Updated */}
-            <p>Price: {productDetail.item.price}</p>
-          </div>
-        )}
-      </main>
-      {showScanner && (
-        <div className="scanner-modal">
-          <div className="scanner-content">
-            <div id="qr-reader" ref={scannerRef}></div>
-            <button className="close-button" onClick={() => setShowScanner(false)}>Close</button>
-          </div>
+    <header className="home-header">
+      <Image src="/pics/BuildStation-logo.png" alt="Logo" width={150} height={150} className="logo"/> {/* Updated */}
+    </header>
+    <main className="home-main">
+      <form onSubmit={handleSearch} className="sku-form">
+        <input type="text" name="sku" value={sku} onChange={handleSkuChange} />
+        <button type="button" className="scan-button" onClick={() => setShowScanner(true)}>Scan Barcode</button>
+      </form>
+      {error && <p>{error}</p>}
+      {productDetail && productDetail.item && (
+        <div>
+          <h2>{productDetail.item.title}</h2>
+          <Image src={productDetail.item.image} alt={productDetail.item.title} width={150} height={150}/> {/* Updated */}
+          <p>Price: {productDetail.item.price}</p>
         </div>
       )}
-    </div>
+    </main>
+    {showScanner && (
+      <div className="scanner-modal">
+        <div className="scanner-content">
+          <video ref={videoRef} style={{ width: '100%' }}></video>
+          <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+          <button className="close-button" onClick={() => {
+            setShowScanner(false);
+            videoRef.current.pause();
+            if (videoRef.current.srcObject) {
+              videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+            }
+          }}>Close</button>
+        </div>
+      </div>
+    )}
+  </div>
   );
 }
+
