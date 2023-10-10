cd7fe5634368f27e119a3e1fd0394f28f9a2d6b3
A1cy <a1hvdy@gmail.com>
2023-10-10 09:01:12 +0300

    ttt

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Html5Qrcode } from 'html5-qrcode';
@@ -11,6 +12,8 @@ export default function Home() {
   const [productDetail, setProductDetail] = useState(null);
   const [showScanner, setShowScanner] = useState(false);
   const scannerRef = useRef(null);
+
+  const [scannerActive, setScannerActive] = useState(false); // New state variable
 
   const handleSkuChange = (e) => {
     setSku(e.target.value);
@@ -42,12 +45,14 @@ export default function Home() {
 
   useEffect(() => {
     const scanner = scannerRef.current;
+    console.log("Scanner element:", scanner); 
     let html5QrCode;
     if (showScanner && scanner) {
-      console.log("Initializing scanner...");  // Debugging line
+      console.log("Scanner dimensions:", scanner.offsetWidth, scanner.offsetHeight);  // Log dimensions
       Html5Qrcode.getCameras().then(devices => {
         console.log("Devices found:", devices);  // Debugging line
         if (devices && devices.length) {
+          // ... (camera selection logic)
           const backCamera = devices.find(device => device.label.toLowerCase().includes('back'));
           const cameraId = backCamera ? backCamera.id : devices[0].id;
           console.log("Using camera:", cameraId);  // Debugging line
@@ -55,8 +60,9 @@ export default function Home() {
           html5QrCode.start(
             cameraId,
             {
-              fps: 20,
-              qrbox: 350,
+              // ... (scanner configuration)
+              fps: 15,
+              qrbox: 300,
               aspectRatio: 1.0, // Set aspect ratio to be square
               formatsToSupport: [ 
                 Html5QrcodeSupportedFormats.QR_CODE,
@@ -75,38 +81,47 @@ export default function Home() {
                 Html5QrcodeSupportedFormats.UPCA,
                 Html5QrcodeSupportedFormats.UPCE,
                 Html5QrcodeSupportedFormats.UPCEAN_EXTENSION,
-              
-              
               ]
             },
             qrCodeMessage => {
               console.log("QR Code scanned:", qrCodeMessage);  // Debugging line
               setSku(qrCodeMessage);
               handleSearch();
-              html5QrCode.stop();
+              if (html5QrCode && scannerActive) { // Conditional stop
+                html5QrCode.stop().then(() => {
+                  setScannerActive(false); // Set scanner inactive upon successful stop
+                }).catch(err => {
+                  console.error("Error stopping the scanner", err);
+                });
+              }
               setShowScanner(false);
             },
             errorMessage => {
-              console.error("Scanner error:", errorMessage);  // Debugging line
+              console.error("Scanner error:", errorMessage);
               setError(errorMessage);
             }
-          ).catch(err => {
-            console.error("Scanner initialization error:", err);  // Debugging line
+          ).then(() => {
+            setScannerActive(true); // Set scanner active upon successful start
+          }).catch(err => {
             setError(err);
           });
         }
       }).catch(err => {
-        console.error("Error getting devices:", err);  // Debugging line
         setError("Error initializing scanner");
       });
     }
     return () => {
-      if (html5QrCode) {
-        html5QrCode.stop();
+      if (html5QrCode && scannerActive) { // Conditional stop
+        html5QrCode.stop().then(() => {
+          setScannerActive(false); // Set scanner inactive upon successful stop
+        }).catch(err => {
+          console.error("Error stopping the scanner", err);
+        });
       }
     };
   }, [showScanner, handleSearch]);
 
+  // ... (render logic)
   return (
     <div className="container">
       <header className="home-header">
@@ -137,6 +152,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-
