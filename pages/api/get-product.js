2dec4192328fe86cb3326775f66a52169ab7623a
a1cy <a1hvdy@gmail.com>
2024-05-13 10:52:48 +0300

    chore: Add form-data dependency and update API request in get-product.js

--- pages/api/get-product.js
+++ pages/api/get-product.js
@@ -1,40 +1,37 @@
-import axios from "axios";
+import FormData from 'form-data';
+import axios from 'axios';
 
 export default async function handler(req, res) {
   const { sku } = req.body;
 
+  // Create a new instance of FormData
+  const formData = new FormData();
+  formData.append('searchText', sku);
+
   try {
     const searchResponse = await axios.post(
       "https://staging-ksa-v2.build-station.com/build-station-apis-v2/api/items/search-items-ajax",
-      { searchText: sku },
+      formData,
       {
         headers: {
-          "Content-Type": "application/json",
-          apikey: "Ujf4ZHdP3zHrmfsvAETpUmvK7BPS/jU/YKCp+VXaF1A=",
-          lang: "en",
-          currency: "SAR",
-          country: "SA",
+          ...formData.getHeaders(), // Spread the headers required by the FormData instance
+          'apikey': "Ujf4ZHdP3zHrmfsvAETpUmvK7BPS/jU/YKCp+VXaF1A=",
+          'lang': "en",
+          'currency': "SAR",
+          'country': "SA",
         },
       }
     );
-    console.log("Search Response:", searchResponse.data);
 
-    if (
-      searchResponse.data &&
-      searchResponse.data.data &&
-      searchResponse.data.data.items &&
-      searchResponse.data.data.items[0]
-    ) {
+    if (searchResponse.data && searchResponse.data.data && searchResponse.data.data.items && searchResponse.data.data.items.length > 0) {
       const product = searchResponse.data.data.items[0];
-      const { slug, item_category_slug: category, uuid } = product; // Include uuid
-
-      // Directly return the product data
-      res.status(200).json({ slug, category, uuid }); // Include uuid in the response
+      const { slug, item_category_slug: category, uuid } = product;
+      res.status(200).json({ slug, category, uuid });
     } else {
       throw new Error("Product not found");
     }
   } catch (error) {
-    console.error("Error in get-product-detail:", error.message);
-    res.status(500).json({ error: "Error fetching product detail" });
+    console.error("Error in get-product:", error);
+    res.status(500).json({ error: "Error fetching product" });
   }
 }
